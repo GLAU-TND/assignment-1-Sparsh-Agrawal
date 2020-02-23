7ac64dc0714b878c180292888d650cf2c4a7bfba
Sparsh-Agrawal <agrawal.sparsh76@gmail.com>
2020-02-23 22:19:32 +0530

    updated

---
+++ SContacts/src/def/MyContacts.java
@@ -0,0 +1,27 @@
+package def;
+
+import adt.MyContactsADT;
+
+public class MyContacts implements MyContactsADT
+{
+
+    @Override
+    public void addContact() {
+
+    }
+
+    @Override
+    public void viewContacts() {
+
+    }
+
+    @Override
+    public void searchContact() {
+
+    }
+
+    @Override
+    public void deleteContact() {
+
+    }
+}
---
+++ SContacts/src/def/MyLinkedList.java
@@ -0,0 +1,22 @@
+package def;
+
+import adt.ListADT;
+
+public class MyLinkedList implements ListADT
+{
+
+    @Override
+    public void add(Object item) {
+        
+    }
+
+    @Override
+    public Object getData(int index) {
+        return null;
+    }
+
+    @Override
+    public Object remove() {
+        return null;
+    }
+}
